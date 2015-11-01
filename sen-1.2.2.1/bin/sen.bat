a47fb317ccadd361729fe5c6cbf033f386acc960
kaiba <kaiba@kaiba-no-Mac-mini.local>
2015-11-01 21:37:16 +0900

    delete unused files

--- sen-1.2.2.1/bin/sen.bat
+++
@@ -1,5 +0,0 @@
-rem set classpath
-SET CLASSPATH=%SEN_HOME%\lib\sen.jar
-SET CLASSPATH=%CLASSPATH%;%SEN_HOME%\lib\commons-logging.jar
-
-@%JAVA_HOME%\bin\java -Dsen.home=%SEN_HOME% -classpath %CLASSPATH% StringTaggerDemo ${1+"$@"}
